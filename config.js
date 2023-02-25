93c97398e7525c5719cad1fee19d73d33a0f464e
André Abreu de Sá <andre.abreu.ufmg@gmail.com>
2023-02-25 15:50:12 +0000

    "feat: Kucoin pullback build"

--- config.js
+++ config.js
@@ -1,52 +1,10 @@
 module.exports = {
 
-    SYMBOL: "BCH-PERP",
- 
+    SYMBOL:'ADA3L/USDT',
+    API_KEY:'63f2735ff2ac430001940065',
+    SECRET_KEY:'bfb1cff6-b2d0-457e-af6a-08cb4c49e7f6',
+    PASSWORD:'#Andreevc3686',
+    UID:'113700999',
+    CRAWLER_INTERVAL:10000
     
-    CRAWLER_INTERVAL : 10000,
-
-    BEAR_PROFITY : 1.10,
-    BEAR_STOP : 0.80,
-    HALF_PROFITY : 1.055,
-    FULL_PROFITY : 1.235,
-    HALF_STOP : 0.97,
-    FULL_STOP : 0.915,
-
-    AVAX_PROFITY : 1.02,
-    AVAX_STOP : 100.0,
-
-    BUY_PROFITY15 : 1.02,
-    SELL_PROFITY15 : 0.95,
-
-    BUY_PROFITY60 : 1.01,
-    SELL_PROFITY60 : 0.86,
-
-    BUY_PROFITYD : 1.02,
-    SELL_PROFITYD : 0.001,
-
-    
-    POSITION_SIZE: 2,
-    NUM_BUY_GRID_LINES: 5,
-    NUM_SELL_GRID_LINES: 5,
-    GRID_SIZE: 0.005,
-    CHECK_ORDERS_FREQUENCY: 10,
-    CLOSED_ORDER_STATUS: 'closed',
-
-    
-    API_KEY15:'fDhvBqPiAMLxlQZgjKdksdRKO3y9y1P07Ebtf6Fd',
-    SECRET_KEY15:'Ru0DmmYIVmxiQZ4nZ75z_P6tAdDoV351Q2qYMq78',
-
-    
-    API_KEY:"tyv6hUyY5QoMqx0Zg8coaMqDO5nsLeBK-CKYobcJ",
-    SECRET_KEY:'DjT6L28adBi-7IiL1sVeVjt2TL_QcuOpKC3kmYOE',
-
-    
-    API_KEY60:'YwQ2E2gsuhV_EoaBRyACSRKkftrxPQ34aX_vbMaR',
-    SECRET_KEY60:'q8-jUYb-oACbFrZGyCyvimIHkRKLilDPqRB5BUrq',
-
-   
-    API_KEYd:'K_hbGUmbXhAoLsnH91_9YSCHd2Yn6umrgb4Z9pty',
-    SECRET_KEYd:'h9WnYUWmR2mn8Mfx-OQYSLt8tXRHMWPBGXwFBcMG'
-    
-
 };
--- strategy/Kucoin.js
+++ strategy/Kucoin.js
@@ -1,164 +1,113 @@
 var ccxt = require('ccxt');
 var config = require('../config');
 var ta = require('ta.js')
-const https = require('https');
 const axios = require('axios');
-const express = require('express');
 const cors = require('cors');
-const { SYMBOL } = require('../config');
 const serverless = require("serverless-http");
 
 
 
-const app = express();
-const port = process.env.PORT || 3001 ;
+async function pullBack() {
+    
+    console.log('');
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
-    app.use(cors());
-    next();
-});
-
-/*  */
-let bought = false;
-let comprado = false;
-let comp = false;
-let Katta = false;
-let buyed = false;
-
-let selled = false;
-let sold = false;
-let vendido = false;
-let Ureta = false;
-let vend = false;
-const symbol = config.SYMBOL;
-
-
-
-async function server4bot() {
-
-    console.log('')
-    /* SISTEMA AVAX/BTC */
-
+    /* SYSTEM */
     var exchange = new ccxt.kucoin({
-        'apiKey': "63f2735ff2ac430001940065",
-        'secret': "bfb1cff6-b2d0-457e-af6a-08cb4c49e7f6",
-        'password': "#Andreevc3686",
-        'uid': "113700999"
+        'apiKey': config.API_KEY,
+        'secret': config.SECRET_KEY,
+        'password': config.PASSWORD,
+        'uid': config.UID
     });
 
+    let comprado = false;
+
     /* DADOS */
-    const mercado   = await exchange.load_markets ();
-    const data      = (await exchange.fetchOHLCV ('ADA3L/USDT', '15m'));
+    const mercado = await exchange.load_markets();
+    const data = (await exchange.fetchOHLCV(config.SYMBOL, '15m'));
+
 
     /* ACESSANDO CANDLES OHLC BCHBEAR/USD */
-    const open  = (data.map(candleO => parseFloat(candleO[1]))).reverse();
-    const high  = (data.map(candleH => parseFloat(candleH[2]))).reverse();
-    const low   = (data.map(candleL => parseFloat(candleL[3]))).reverse();
-    const close = (data.map(candleC => parseFloat(candleC[4]))).reverse();
+    const open = (data.map(candleO => (candleO[1]))).reverse();
+    const high = (data.map(candleH => (candleH[2]))).reverse();
+    const low = (data.map(candleL => (candleL[3]))).reverse();
+    const close = (data.map(candleC => (candleC[4]))).reverse();
 
     /* MÉDIAS DE MÓVEIS */
-    const fastMedian  = await ta.sma(low, 88);
-    const slowMedian  = await ta.sma(high, 100);
+    const fastMedian = await ta.sma(low, 88);
+    const slowMedian = await ta.sma(high, 100);
     const threeMedian = await ta.sma(high, 3);
 
-//     /* RSI 15m BCHBEAR */
-
-//     const param = low;
-//     const src = 30;    
-
-//     function calcRSI(param) {
-//         let gains = 0;
-//         let losses = 0;
-    
-//         for (let i = param.length - src; i < param.length; i++) {
-//             const diff = param[i] - param[i - 1];
-//             if (diff >= 0)
-//                 gains += diff;
-//             else
-//                 losses -= diff;
-//         }
-    
-//         const strength = gains / losses;
-//         return 100 - (100 / (1 + strength))
-//     }
-
-//     const rsi = calcRSI(param);
-
-    
-//     /* CONVERÇÃO DE MOEDAS */ 
+    /* CONVERÇÃO DE MOEDAS */
     const saldo = await exchange.fetchBalance();
-    const USDTTotal     = (saldo.total['USDT']);
-    const USDTFree      = (saldo.free['USDT']);
-    const ADA3LTotal    = (saldo.total['ADA3L']);
-    const BTCtotal      = ((saldo.total['BTC']));
+    const USDTTotal = saldo.total['USDT'];
+    const USDTFree = saldo.free['USDT'];
+    const ADA3LTotal = saldo.total['ADA3L'];
+    const BTCtotal = saldo.total['BTC'];
+    const amountUSDT = 1.1
 
+    /* CRIAÇÃO DE PROFITS */
+    const trades = (await exchange.fetchMyTrades('ADA3L/USDT')).reverse();
+    const groundZero = trades.find(trades => trades.side === 'sell').timestamp
+    const point = trades.filter(trades => trades.side === 'buy')
+    const amount = point.filter(trades => trades.timestamp > groundZero)
+    const setPrice = amount.map(amount => amount.price)
+    const n = setPrice.length;
+    let sum = 0;
 
- 
+    for (var i = 0; i < setPrice.length; i++) {
+        sum += setPrice[i];
+    }
 
-//     /* ANALISE DE WALLET */
-//     let quantity = (USDTFree);
+    const medianPrice = sum / n
 
-    // /* CRIAÇÃO DE PROFITS */ 
-    // const trades = (await exchange.fetchOrders('ADA3L/USDT')).reverse(); 
-    // const Profit  = parseFloat((trades[0].price)*1.02);
-    // const Stop    = parseFloat((trades[0].price));
+    const Profit = (medianPrice * 1.02);
 
 
     /* CRUZAMENTO DE MEDIAS */
-    const crossover = (fastMedian[1]>slowMedian[1] && fastMedian[2]<slowMedian[2]);
-    const crossunder = (fastMedian[1]<slowMedian[1] && fastMedian[2]>slowMedian[2]);
+    const crossover = (fastMedian[1] > slowMedian[1] && fastMedian[2] < slowMedian[2]);
+    const crossunder = (fastMedian[1] < slowMedian[1] && fastMedian[2] > slowMedian[2]);
 
     /* MOMENTO DO TRADE */
-    //const tstamp    = parseFloat(trad[0].timestamp);
-    const timer     = (1000*60*240) 
+    const timer = (1000 * 60 * 29);
+    const tstamp = ((trades[0].timestamp)+timer);
+    const currentCandle = data[0][0]
 
-//     /* REGISTRO DE MAREGM LIVRE */
-//     const lado   = trades[0].side;
-//     const ativo  = trades[0].symbol
+    /* REGISTRO DE MAREGM LIVRE */
+    const lado = trades[0].side;
+    const ativo = trades[0].symbol;
+    const bodyTrade = {
+        orderId: trades[0].order,
+        symbol: trades[0].symbol,
+        side: trades[0].side,
+        quantity: trades[0].amount,
+        priceUSD: trades[0].price,
+        timestamp: trades[0].timestamp,
+        moment: trades[0].datetime,
+    }
 
 
-//     if(lado === "buy" && ativo === 'ADA3L/USDT' && (USDTFree < (ADA3LTotal*close[0]))){
-//         comprado = true;
-//         console.log('Comprado em ADA3L')
-//         console.log(`Profit em ${Profit}`)
+    if (lado === "buy" && ativo === 'ADA3L/USDT' && (0.999999 < (ADA3LTotal * close[0]))) {
+        comprado = true;
+        console.log('Comprado em ADA3L');
+        console.log(`Profit em ${Profit}`);
 
-//     }else{
-//         vendido = false;
-//     }
+    } else {
+        vendido = false;
+    }
 
-
-//     /* ESTATÉGIAS , CONDIÇÕES E ORDENS  */
- 
-    if(close[3]<open[3]  && close[2]<open[2] && close[1]>open[1] && ((close[1]-open[1])>(open[1]-low[1]))){
+    /* ESTATÉGIAS , CONDIÇÕES E ORDENS  */
+    if (currentCandle>tstamp && close[3] < open[3] && close[2] < open[2] && close[1] > open[1] && ((close[1] - open[1]) > (open[1] - low[1]))) {
         console.log("Compra ADA3L")
-        var buy = exchange.createMarketBuyOrder('ADA3L/USDT', USDTFree);
-    } 
+        var buy = exchange.createMarketBuyOrder('ADA3L/USDT', amountUSDT);
+    }
 
-    if(comprado && (((close[0])>=Profit) && ativo === 'ADA3L/USDT' && lado === 'buy')){
+    if (comprado && ((close[0] >= Profit) && ativo === 'ADA3L/USDT' && lado === 'buy')) {
         console.log("Fechamento do trade")
-        var sell = exchange.createMarketSellOrder('ADA3L/USD', ADA3LTotal);
-    } 
-
-    app.get('/', async(req, res) => {
-        return res.json({
-            erro: false,
-            datahome: {
-                data
-            }
-        });
-    });
-
+        var sell = exchange.createMarketSellOrder('ADA3L/USDT', ADA3LTotal);
+    }
+    
 }
 
+module.exports =  { pullBack } ;
 
-module.exports =  { server4bot } ;
-
-
-app.listen(port, () => {
-    console.log(`Servidor iniciado na porta: ${port}`);
-});
-
-setInterval(server4bot, config.CRAWLER_INTERVAL);
+setInterval(pullBack, config.CRAWLER_INTERVAL);
